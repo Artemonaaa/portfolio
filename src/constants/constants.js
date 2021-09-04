d07651bb8cbf69b141fb05411ee31cbada044b31
Artemona <artemidaabc@gmail.com>
2021-09-04 23:53:57 +0300

    fix images

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -2,7 +2,7 @@ export const projects = [
   {
     title: 'Mi band',
     description: "Online watch store, with very large functionality and its own admin panel. The biggest project I worked on.",
-      image: '/images/1.png',
+      image: '/images/1.PNG',
       tags: ['MongoDB', 'Express', 'React', 'Node', 'Redux'],
     source: 'https://smart-electronix.herokuapp.com/', 
     visit: 'https://github.com/LeoSame/fe-20_final_project',
@@ -20,7 +20,7 @@ export const projects = [
   {
     title: 'The Ham',
     description: "Common markup without adaptive. Using various libraries for JavaScript",
-      image: '/images/3.png',
+      image: '/images/3.PNG',
       tags: ['HTML', 'CSS', 'JavaScript'],
     source: 'https://artemonaaa.github.io/',
     visit: 'https://github.com/Artemonaaa/artemonaaa.github.io',
@@ -29,7 +29,7 @@ export const projects = [
   {
     title: 'Forkio',
     description: "This is simple program whith adaptive web design",
-    image: '/images/4.png',
+    image: '/images/4.PNG',
     tags: ['HTML', 'Gulp', 'SCSS'],
     source: 'https://vgodlevskyi.github.io/StepProject_Forkio/',
     visit: 'https://gitlab.com/Denys_Shevchuk/dan-it-adaptive_step_project',
